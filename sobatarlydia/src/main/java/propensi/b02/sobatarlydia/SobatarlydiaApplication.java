233a86007d71f6f2fd4460d216622815c775d627
alifiyah <alifiyah.nur@ui.ac.id>
2023-02-26 19:12:10 +0700

    Project Initialization

---
+++ sobatarlydia/src/main/java/propensi/b02/sobatarlydia/SobatarlydiaApplication.java
@@ -0,0 +1,13 @@
+package propensi.b02.sobatarlydia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SobatarlydiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SobatarlydiaApplication.class, args);
+	}
+
+}
